7f9a25b9872fad97e2a1684af00cd7908880185c
Kayley <kayley.luftig@gmail.com>
2019-05-02 16:27:26 +0000

    find index of waldo with for loop

---
+++ callbacks-intro/find-waldo.js
@@ -0,0 +1,27 @@
+/*
+Modify the callback function in the previous example
+so that logs the index of the array where Waldo is found,
+ie. "Found Waldo at index 2!".
+(You will need to modify actionWhenFound to receive the index.)
+*/
+function findWaldo(arr, found) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "Waldo") {
+      found(i);   // execute callback
+    }
+  }
+}
+
+function actionWhenFound(position) {
+  console.log("Found him at index " + position);
+}
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+
+
+
+// forEach
+// arr.forEach(function(found){
+//   found();
+// });
