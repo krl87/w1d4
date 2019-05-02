d9c2885f361777a9be90ea658f713727d8c538e8
Kayley <kayley.luftig@gmail.com>
2019-05-02 23:01:27 +0000

    using function closures to emmulating random dice roll

--- callbacks-intro/test.js
+++ callbacks-intro/test.js
@@ -1,29 +1,4 @@
-var ghibliMovies = ["Spirited Away", "Princess Mononoke", "My Neighbour Totoro", "Howl's Moving Castle", "Ponyo", "My Neighbours the Yamadas"];
 
-
-function map(arr, f) {
-  var ghibliTitles = [];
-  for (var i = 0; i < arr.length; i++) {
-    ghibliTitles.push(f(arr[i]));
-  }
-  return ghibliTitles;
-}
-
-console.log(
-  map(ghibliMovies, function(word) {
-    return word.length;
-}));
-
-// = ghibliMovies.map(..)
-// [ghibliMovies[0].length, ghibliMovies[1].length]
-
-console.log(
-  map(ghibliMovies, function(word) {
-    return word.toUpperCase();
-}));
-
-console.log(
-  map(ghibliMovies, function(word) {
-    return word.split('').reverse().join('');
-}));
-
+!function() {
+  console.log("Hello from IIFE");
+}();
